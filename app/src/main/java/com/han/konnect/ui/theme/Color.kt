de4de1c4587ba7f58284cd65938b2668d2cefb6e
SeungHwan Lee <asdf26532@gmail.com>
2026-08-07 20:10:50 +0900

    day29

--- app/src/main/java/com/han/konnect/ui/theme/Color.kt
+++ app/src/main/java/com/han/konnect/ui/theme/Color.kt
@@ -2,8 +2,18 @@ package com.han.konnect.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val PurpleMain = Color(0xFF6366F1)
-val PurpleLight = Color(0xFFEEF2FF)
-val TextDark = Color(0xFF0F172A)
-val TextGray = Color(0xFF64748B)
-val BgFluid = Color(0xFFF8FAFC)
+// Light Theme Colors
+val PurpleMain = Color(0xFF6C5CE7)
+val PurpleDark = Color(0xFF5B4BC4)
+val PurpleLight = Color(0xFFA29BFE)
+
+val PrimaryBackgroundLight = Color(0xFFF8FAFC)
+val SurfaceLight = Color(0xFFFFFFFF)
+val TextPrimaryLight = Color(0xFF1E293B)
+val TextSecondaryLight = Color(0xFF64748B)
+
+// Dark Theme Colors
+val PrimaryBackgroundDark = Color(0xFF0F172A)
+val SurfaceDark = Color(0xFF1E293B)
+val TextPrimaryDark = Color(0xFFF8FAFC)
+val TextSecondaryDark = Color(0xFF94A3B8)
